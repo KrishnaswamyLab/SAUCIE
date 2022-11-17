1880bec62544275b2824200b5b552eebf83e89a1
amrukwa <a.mrukwa00@gmail.com>
2022-11-17 23:23:03 +0100

    fix rounding

--- streamlit_elements/scores.py
+++ streamlit_elements/scores.py
@@ -17,12 +17,15 @@ def compute_silhouette(x, y):
 def display_scores(X, X_embed, y_pred, y_true=None):
     col1, col2 = st.columns(2)
     silhouette_labels = compute_silhouette(X, y_pred)
+    silhouette_labels = round(silhouette_labels, 5)
     col1.metric("Silhouette of the clustering",
-                value=round(silhouette_labels, 5))
+                value=silhouette_labels)
     if y_true is not None:
         ari = compute_ari_score(y_pred, y_true)
+        ari = round(ari, 5)
         silhouette_embed = compute_silhouette(X_embed, y_true)
+        silhouette_embed = round(silhouette_embed, 5)
         col2.metric("Adjusted Rand Index",
                     value=round(ari, 5))
         col1.metric("Silhouette of the embedding",
-                    value=round(silhouette_embed, 5))
+                    value=silhouette_embed)
