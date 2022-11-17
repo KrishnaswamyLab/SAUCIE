9ed64a244ef278b05943738a9c20c945fbe066b0
amrukwa <a.mrukwa00@gmail.com>
2022-11-17 16:59:17 +0100

    check dimensions

--- test/test_saucie.py
+++ test/test_saucie.py
@@ -24,13 +24,39 @@ def data_batches():
     data = np.random.randint(0, 3, 10000)
     return data
 
-# def test_SAUCIE_compresses_data():
+
+def test_SAUCIE_compresses_data():
+    data = data_saucie()
+    saucie = SAUCIE_labels(epochs=2, lr=1e-6, normalize=True, random_state=42)
+    saucie.fit(data)
+    encoded = saucie.transform(data)
+    assert encoded.shape == (10000, 2)
+
+
+def test_SAUCIE_batches_preserves_data_shape():
+    data = data_saucie()
+    batches = data_batches()
+    saucie = SAUCIE_batches(epochs=2, lr=1e-9, normalize=True, random_state=42)
+    saucie.fit(data, batches)
+    cleaned = saucie.transform(data, batches)
+    assert cleaned.shape == (10000, 20)
+
+
+# def test_SAUCIE_batches_yields_stable_results_batches_order():
 #     assert 0 == 1
 
 
-# def test_SAUCIE_batches_compresses_data():
-#     assert 0 == 1
+def test_SAUCIE_labels_data():
+    data = data_saucie()
+    saucie = SAUCIE_labels(epochs=8, lr=1e-6, normalize=True,
+                           random_state=42)
+    saucie.fit(data)
+    labels = saucie.predict(data)
+    assert labels.shape == (10000, )
 
+
+# def test_SAUCIE_batches_preserves_ref_batch():
+#     assert 0 == 1
 
 # def test_SAUCIE_yields_stable_results_without_training():
 #     assert 0 == 1
@@ -50,7 +76,7 @@ def data_batches():
 
 def test_SAUCIE_is_clonable():
     data = data_saucie()
-    saucie = SAUCIE_labels(epochs=3, lr=1e-6, normalize=True, random_state=42)
+    saucie = SAUCIE_labels(epochs=2, lr=1e-6, normalize=True, random_state=42)
     saucie.fit(data)
     labels1 = saucie.predict(data)
     encoded1 = saucie.transform(data)
@@ -77,18 +103,6 @@ def test_SAUCIE_batches_is_clonable():
     np.testing.assert_array_equal(cleaned1, cleaned2)
 
 
-# def test_SAUCIE_batches_yields_stable_results_batches_order():
-#     assert 0 == 1
-
-
-# def test_SAUCIE_labels_data():
-#     assert 0 == 1
-
-
-# def test_SAUCIE_batches_preserves_ref_batch():
-#     assert 0 == 1
-
-
 def test_SAUCIE_is_picklable():
     obj = SAUCIE_labels()
     with io.BytesIO() as f:
@@ -109,7 +123,7 @@ def test_SAUCIE_batches_is_picklable():
 
 def test_SAUCIE_pickling_restores_tf_graph():
     data = data_saucie()
-    saucie = SAUCIE_labels(epochs=3, lr=1e-6, normalize=True, random_state=42)
+    saucie = SAUCIE_labels(epochs=2, lr=1e-6, normalize=True, random_state=42)
     saucie.fit(data)
     labels1 = saucie.predict(data)
     encoded1 = saucie.transform(data)
